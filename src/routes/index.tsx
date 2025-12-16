c8b2f53dc0a6a37c7dfc6e838e7ad1c076c3ff39
Kasun Lakshitha <kasun2014@outlook.com>
2025-12-16 22:21:04 +0530

    Add MonacoEditorComponent and update import paths in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,23 @@
-import { createFileRoute } from '@tanstack/react-router';
-import MonacoEditorComponent from '@/components/monaco-editor';
+import { createFileRoute } from "@tanstack/react-router";
+import MonacoEditorComponent from "../components/MonacoEditorComponent";
 
-export const Route = createFileRoute('/')({ component: App });
+export const Route = createFileRoute("/")({ component: App });
 
 function App() {
-	return (
-		<div className='flex flex-row gap-4'>
-			<MonacoEditorComponent
-				value={() => {
-					return 'Hello World';
-				}}
-				onChange={() => {}}
-			/>
-			<MonacoEditorComponent
-				value={() => {
-					return 'Hello World';
-				}}
-				onChange={() => {}}
-			/>
-		</div>
-	);
+  return (
+    <div className="flex flex-row gap-4">
+      <MonacoEditorComponent
+        value={() => {
+          return "Hello World";
+        }}
+        onChange={() => {}}
+      />
+      <MonacoEditorComponent
+        value={() => {
+          return "Hello World";
+        }}
+        onChange={() => {}}
+      />
+    </div>
+  );
 }
