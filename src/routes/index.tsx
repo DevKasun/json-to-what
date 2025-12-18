76d9a69dbf42b9abfd2432438f315297bd30b10e
Kasun Lakshitha <kasun2014@outlook.com>
2025-12-18 20:36:56 +0530

    Refactor router setup, remove MonacoEditorComponent and demo data, and update index.tsx to use Monaco Editor directly

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,23 @@
-import { createFileRoute } from "@tanstack/react-router";
-import MonacoEditorComponent from "../components/MonacoEditorComponent";
+import { createFileRoute } from '@tanstack/react-router';
+import Editor from '@monaco-editor/react';
 
-export const Route = createFileRoute("/")({ component: App });
+export const Route = createFileRoute('/')({ component: App });
 
 function App() {
-  return (
-    <div className="flex flex-row gap-4">
-      <MonacoEditorComponent
-        value={() => {
-          return "Hello World";
-        }}
-        onChange={() => {}}
-      />
-      <MonacoEditorComponent
-        value={() => {
-          return "Hello World";
-        }}
-        onChange={() => {}}
-      />
-    </div>
-  );
+	return (
+		<div className='flex flex-row gap-4'>
+			<div className='w-1/2'>
+				<h3 className='text-lg font-bold pb-2'>JSON</h3>
+				<Editor height='70vh' defaultLanguage='json' theme='vs-dark' />
+			</div>
+			<div className='w-1/2'>
+				<h3 className='text-lg font-bold pb-2'>TypeScript</h3>
+				<Editor
+					height='70vh'
+					defaultLanguage='typescript'
+					theme='vs-dark'
+				/>
+			</div>
+		</div>
+	);
 }
